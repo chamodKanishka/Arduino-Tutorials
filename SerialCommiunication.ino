06305b616bb4a57e4df4cbf4aceed98924916490
chamodKanishka <chamodkanishka77@gmail.com>
2019-12-21 07:04:54 -0800

    Serial commiunication
    test how actually serial commiunication works

---
+++ SerialCommiunication.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  float x;
+  x =10.523;
+  Serial.println(x);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
